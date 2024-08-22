709eb49da5aeab031ec53cbea5c5aba70d5465dd
Hillson Shalom <hillshaloz@gmail.com>
2024-08-22 14:39:11 +0300

    updating the requirements

--- FinalProject/Services/IServices/IMissionService.cs
+++ FinalProject/Services/IServices/IMissionService.cs
@@ -1,13 +1,17 @@
 ﻿using FinalProjectAPI.Models;
+using FinalProjectAPI.Models.AuxiliaryModels;
 using FinalProjectAPI.Models.BaseModels;
 
 namespace FinalProjectAPI.Services.IServices
 {
     public interface IMissionService
     {
+        Task<MissionPK> Create(int agentId, int targetId);
+        Task<MissionPK> Create(Agent agent, Target target);
         Task DirectMission();
         Task<IEnumerable<Mission>> GetMissions();
         Task<IEnumerable<Mission>> OfferMissions();
-        Task ChangeStatus(MissionStatus status);
+        Task<bool> UpdateStatus(int id, MissionStatus status);
+        Task<bool> UpdateStatus(Mission mission, MissionStatus status);
     }
 }
