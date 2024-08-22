c1a15ccfb83845fb7c87c79ff9a27083fd7a14c6
Hillson Shalom <hillshaloz@gmail.com>
2024-08-22 17:34:55 +0300

    some changes

--- FinalProject/Services/IServices/IMissionService.cs
+++ FinalProject/Services/IServices/IMissionService.cs
@@ -8,6 +8,7 @@ namespace FinalProjectAPI.Services.IServices
     {
         Task<MissionPK> Create(int agentId, int targetId);
         Task<MissionPK> Create(Agent agent, Target target);
+        Task Save(Mission mission);
         Task DirectMission();
         Task<IEnumerable<Mission>> GetMissions();
         Task<IEnumerable<Mission>> OfferMissions();
