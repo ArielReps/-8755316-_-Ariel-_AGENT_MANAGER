87bf9c309ef0576cbdb63a36a94058b4c79e539d
Hillson Shalom <hillshaloz@gmail.com>
2024-08-22 14:40:49 +0300

    implementation of the functions that related to the DB, before taking care of functionality

--- FinalProject/Services/MissionService.cs
+++ FinalProject/Services/MissionService.cs
@@ -1,7 +1,9 @@
 ﻿using FinalProjectAPI.Data;
 using FinalProjectAPI.Models;
+using FinalProjectAPI.Models.AuxiliaryModels;
 using FinalProjectAPI.Models.BaseModels;
 using FinalProjectAPI.Services.IServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProjectAPI.Services
 {
@@ -12,9 +14,20 @@ namespace FinalProjectAPI.Services
         {
             _context = db;
         }
-        public Task ChangeStatus(MissionStatus status)
+        public async Task<bool> UpdateStatus(int id, MissionStatus status)
         {
-            throw new NotImplementedException();
+            Mission? mission = await _context.Missions.FindAsync(id);
+            if (mission == null) return false;
+            mission.Status = status;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateStatus(Mission mission, MissionStatus status)
+        {
+            mission.Status = status;
+            await _context.SaveChangesAsync();
+            return true;
         }
 
         public Task DirectMission()
@@ -22,14 +35,51 @@ namespace FinalProjectAPI.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Mission>> GetMissions()
+        public async Task<IEnumerable<Mission>> GetMissions()
         {
-            throw new NotImplementedException();
+            return await _context.Missions.ToListAsync();
         }
 
         public Task<IEnumerable<Mission>> OfferMissions()
         {
             throw new NotImplementedException();
         }
+
+        public async Task<MissionPK> Create(int agentId, int targetId)
+        {
+            Agent? agent = await _context.Agents.FindAsync(agentId);
+            Target? target = await _context.Targets.FindAsync(targetId);
+            if (agent == null || target == null) return new() { AgentId = 0, TargetId = 0 };
+            Mission mission = new()
+            {
+                Agent = agent,
+                AgentId = agent.Id,
+                Target = target,
+                TargetId = target.Id,
+                StartDate = DateTime.Now,
+                Status = MissionStatus.Active
+            };
+            _context.Missions.Add(mission);
+            await _context.SaveChangesAsync();
+            return new() { AgentId = mission.AgentId, TargetId = mission.TargetId };
+        }
+
+        public async Task<MissionPK> Create(Agent agent, Target target)
+        {
+            Mission mission = new()
+            {
+                Agent = agent,
+                AgentId = agent.Id,
+                Target = target,
+                TargetId = target.Id,
+                StartDate = DateTime.Now,
+                Status = MissionStatus.Active
+            };
+            _context.Missions.Add(mission);
+            await _context.SaveChangesAsync();
+            return new() { AgentId = mission.AgentId, TargetId = mission.TargetId };
+        }
+
+        
     }
 }
