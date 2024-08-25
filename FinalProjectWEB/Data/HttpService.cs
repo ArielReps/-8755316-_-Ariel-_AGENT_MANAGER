ce1a5620af9d5df5736c29c29712bd9c3503bdf2
Hillson Shalom <hillshaloz@gmail.com>
2024-08-25 09:55:50 +0300

    a service for http requests

---
+++ FinalProjectWEB/Data/HttpService.cs
@@ -0,0 +1,39 @@
+﻿using FinalProjectWEB.Models;
+using FinalProjectWEB.Models.AuxiliaryModels;
+using FinalProjectWEB.Models.BaseModels;
+
+namespace FinalProjectWEB.Data
+{
+    public class HttpService : IHttpService
+    {
+        public Task<int> ConfirmMission(MissionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Agent>> GetAgentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Mission>> GetMissionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Mission>> GetMissionsOffersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Target>> GetTargetsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Token> Login(string userString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalProjectWEB/Data/IHttpService.cs
@@ -0,0 +1,16 @@
+﻿using FinalProjectWEB.Models;
+using FinalProjectWEB.Models.AuxiliaryModels;
+using FinalProjectWEB.Models.BaseModels;
+
+namespace FinalProjectWEB.Data
+{
+    public interface IHttpService
+    {
+        Task<IEnumerable<Agent>> GetAgentsAsync();
+        Task<IEnumerable<Target>> GetTargetsAsync();
+        Task<IEnumerable<Mission>> GetMissionsAsync();
+        Task<IEnumerable<Mission>> GetMissionsOffersAsync();
+        Task<int> ConfirmMission(MissionStatus status);
+        Task<Token> Login(string userString); 
+    }
+}
