f26e303a94d293923f69d507c7b47cc2a32d6036
Hillson <hillsonshalom@gmail.com>
2024-08-26 18:17:48 +0300

    a service for generating tokens

--- API/FinalProject/Services/AuthService.cs
+++ API/FinalProject/Services/AuthService.cs
@@ -1,18 +1,41 @@
-﻿using FinalProjectAPI.Models.AuxiliaryModels;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace FinalProjectAPI.Services
 {
     public class AuthService
     {
         private readonly IConfiguration _configuration;
-        public AuthService(IConfiguration con)
+        public AuthService(IConfiguration configuration)
         {
-            _configuration = con;
+            _configuration = configuration;
         }
 
-        //public string GenerateToken(LoginModel login)
-        //{
+        public string GenerateToken(string userString)
+        {
+            string key = _configuration.GetValue("Jwt:Key", string.Empty)
+                ?? throw new InvalidOperationException("JWT key is missing");
 
-        //}
+            int expiration = _configuration.GetValue("Jwt:ExpiryInMinutes", 15);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userString)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                //expires: DateTime.Now.AddMinutes(expiration),
+                //claims: claims,
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
