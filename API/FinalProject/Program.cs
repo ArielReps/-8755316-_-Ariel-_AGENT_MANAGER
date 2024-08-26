0d4a33780d827631464b795abe30f7cb3d50a176
Hillson <hillsonshalom@gmail.com>
2024-08-26 18:17:06 +0300

    configuring the Jwt, not finished yet

--- API/FinalProject/Program.cs
+++ API/FinalProject/Program.cs
@@ -1,9 +1,13 @@
 ﻿using FinalProjectAPI.Data;
 using FinalProjectAPI.Services;
 using FinalProjectAPI.Services.IServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Events;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +19,48 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(builder.Configuration));
 
 // הגדרות אימות והרשאה
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
+// לסוואגר. אמור להמחק בסוף
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("V1", new OpenApiInfo { Title = "Final Project API", Version = "V1"});
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "Jwt",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' and then your valid token"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
@@ -31,6 +76,7 @@ builder.Services.AddCors(options =>
 });
 // הוספת סרוויסים
 builder.Services.AddSingleton<ControlService>();
+builder.Services.AddSingleton<AuthService>();
 builder.Services.AddScoped<IAgentService, AgentService>();
 builder.Services.AddScoped<ITargetService, TargetService>();
 builder.Services.AddScoped<IMissionService, MissionService>();
@@ -52,6 +98,7 @@ app.UseCors();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
