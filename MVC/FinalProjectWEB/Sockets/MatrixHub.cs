c1dad5fd78c45f19750662085522222b47783456
Hillson <hillsonshalom@gmail.com>
2024-08-27 09:04:12 +0300

    initial socket service

---
+++ MVC/FinalProjectWEB/Sockets/MatrixHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FinalProjectWEB.Sockets
+{
+    public class MatrixHub : Hub
+    {
+        // לעשות סה"כ לולאה שרצה כל הזמן מרגע שנהיה חיבור ושולחת כל כמה שניות רשימות מעודכנות
+        // לעשות שזה יהיה טאסק שאפשר לבטל ע"י הפונקציה שמופעלת ברגע הניתוק
+    }
+}
