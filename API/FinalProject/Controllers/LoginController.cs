e4a4eb1bd31e4b7d36b7ee7c98c76708687f828c
Hillson <hillsonshalom@gmail.com>
2024-08-27 08:57:52 +0300

    login endpoint completed

--- API/FinalProject/Controllers/LoginController.cs
+++ API/FinalProject/Controllers/LoginController.cs
@@ -11,17 +11,28 @@ namespace FinalProjectAPI.Controllers
     public class LoginController : ControllerBase
     {
         private readonly AuthService _authService;
-        public LoginController(AuthService auth)
+        private readonly ILogger<LoginController> _logger;
+        public LoginController(AuthService auth, ILogger<LoginController> logger)
         {
             _authService = auth;
+            _logger = logger;
         }
         
+        // מחזיר טוקן ע"פ סוג המשתמש
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        public async Task<IActionResult> Login([FromBody] LoginModel userType)
         {
-            // כאן צריך לייצר לו טוקן
-            string token = _authService.GenerateToken(model.Token);
-            return Ok(new { token });
+            AuthModel user = userType.Id switch
+            {
+                "SimulationServer" => new AuthModel { Name = "Simulator Client", Roles = ["Simulator"] },
+                "MVCServer" => new AuthModel { Name = "WEB Server", Roles = ["MVC-Server"] },
+                "test" => new AuthModel { Name = "Test", Roles = ["Test"] },
+                _ => new AuthModel()
+            };
+
+            _logger.LogInformation($"{user.Name ?? "not"} registered");
+            string token = _authService.Create(user); // אם נכנסים עם סוג אחר לא נצליח ליצור טוקן, ובמקרה זה תוחזר מחרוזת ריקה
+            return Ok(new { token = token});
         } 
     }
 }
