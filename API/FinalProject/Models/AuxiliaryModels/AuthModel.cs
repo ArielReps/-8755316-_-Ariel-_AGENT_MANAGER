45a3e3c6fe4afb64c961ea207b3999e9aa64479a
Hillson <hillsonshalom@gmail.com>
2024-08-27 08:59:45 +0300

    service for generating tokens

---
+++ API/FinalProject/Models/AuxiliaryModels/AuthModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalProjectAPI.Models.AuxiliaryModels
+{
+    public class AuthModel
+    {
+        public string Name { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
